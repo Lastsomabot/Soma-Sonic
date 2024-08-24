1a82b6a5a0fac065d30002d5e9fc1840b2632e7e
Lastsomabot <somasama580@gmail.com>
2024-08-25 01:21:02 +0200

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -15,9 +15,9 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, args, api, event }) {
-		const permission = ["61557118090040"];
+		const permission = ["61550545504214"];
 		if (!permission.includes(event.senderID)) {
-			return api.sendMessage(" You don't have permission to use this command. 🐤", event.threadID, event.messageID);
+			return api.sendMessage(" t'as pas le niveau pour utiliser cette cmd. 💢", event.threadID, event.messageID);
 		}
 
 		const fileName = args[0];
